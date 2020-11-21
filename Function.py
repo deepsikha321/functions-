1729b5329e8ffe81d6ac752a5af6cce2f471edcc
deepsikha321 <66194344+deepsikha321@users.noreply.github.com>
2020-11-21 10:03:04 +0530

    Created a file

---
+++ Function.py
@@ -0,0 +1,17 @@
+from collections import Counter 
+
+  
+# initializing string  
+
+test_str = "Mississippi"
+
+  
+# using collections.Counter() to get  
+# count of each element in string  
+
+res = Counter(test_str) 
+
+  
+# printing result  
+
+print ("Count of all characters in GeeksforGeeks is :\n +  str(res)) 
